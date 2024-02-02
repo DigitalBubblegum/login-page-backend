8f8378d310ce55346a0d4e0c42bef92f3fe1f7b4
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-02-02 19:40:05 +0530

    -/login route is now operational

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const userSchema = new mongoose.Schema({
     username:String,
     email:String,
-    password:String
+    passwordHash:String
 })
 userSchema.set('toJSON',{
     transform: (document, returnedObject) => {
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,9 +1,23 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
+const User = require('../models/user')
 
 loginRouter.get('/',(req,res)=>{
     res.send({info:'login router is working'})
 })
-// loginRouter.post('/',async(req,res)=>{
-//     const { username, password } = request.body
-// })
+loginRouter.post('/',async(req,res)=>{
+    const { username, password } = req.body
+    const user = await User.findOne({username})
+    const passwordCorrect = user === null ? false : await bcrypt.compare(password,user.passwordHash)
+    if(!(user && passwordCorrect)){
+        return res.status(401).json({error:'invalid username or password'})
+    }
+    const userForToken = {
+        username: user.username,
+        id: user._id,
+    }
+    const token = jwt.sign(userForToken, process.env.SECRET)
+    res.status(200).send({token,username:user.username,email:user.email})
+})
 module.exports = loginRouter
