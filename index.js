0871528238b5d2378cb3f0767216b68dd6ccaa80
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-02-02 19:22:00 +0530

    -/signup endpoint is now functional. we can create a new user.

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const loginRouter = require('./src/routes/login');
 const signupRouter = require('./src/routes/signup');
 require('dotenv').config()
 const PORT = process.env.PORT;
+const url = process.env.URL;
 const app = express()
+const mongoose = require('mongoose')
+mongoose.set('strictQuery',false)
+mongoose
+	.connect(url)
+	.then(() => {
+		console.log('connected to MongoDB')
+	})
+	.catch((error) => {
+		console.log('error connecting to MongoDB:', error.message)
+	})
+app.use(express.json())
 app.get('/v1/',(req,res) => {
     res.send({info:'This is the v1 version of the endpoint'})
 })
--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,7 +1,21 @@
 const bcrypt = require('bcrypt')
 const signupRouter = require('express').Router()
 // Add the user model here
+const User = require('../models/user')
 signupRouter.get('/',(req,res)=>{
     res.send({info:'signup router is working'})
+})
+signupRouter.post('/',async(req,res) =>{
+    const {username,email,password} = req.body
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password,saltRounds)
+    const user = new User({
+        username,
+        email,
+        passwordHash
+    })
+    const savedUser = await user.save()
+    console.log('saved new user')
+    res.status(201).json(savedUser)
 })
 module.exports = signupRouter
